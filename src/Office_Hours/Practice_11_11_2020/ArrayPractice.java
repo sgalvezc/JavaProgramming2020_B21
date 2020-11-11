9cd1e234055039397ab4b6b364df92cca1ac83ed
Stephanie <stephanie.galvez.c@gmail.com>
2020-11-11 15:24:24 -0500

    Office Hours - single dimensional array

---
+++ src/Office_Hours/Practice_11_11_2020/ArrayPractice.java
@@ -0,0 +1,46 @@
+package Office_Hours.Practice_11_11_2020;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 8, 4, 5};
+        int[] arr2 = {4, 5, 6, 7, 8};
+
+        for (int j = 0; j <= arr1.length - 1; j++) { //iterates the arr1' indexes
+            int element = arr1[j]; //1
+            for (int i = 0; i <= arr2.length - 1; i++) { //iterates the arr2' indexes
+                if (element == arr2[i]) {
+                    System.out.println(element);
+                }
+            }
+        }
+
+        System.out.println("==========================");
+
+        for(int element : arr1 ){
+
+            for(int each : arr2){
+                if(each == element){
+                    System.out.println(element);
+                }
+            }
+
+        }
+
+
+
+    }
+}
+
+/*
+Write a program that can print out the common elements from two integer array
+		Ex:
+			arr1: {1,2,3,4,5}
+			arr2: {4,5,6,7,8}
+			output:
+				4 5
+	solution 1:
+			DO NOT use any for each loop
+	solution 2:
+			MUST use for each loops
+ */
